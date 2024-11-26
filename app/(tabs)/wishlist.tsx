111d6830566a8b84105d94e3aa21356dd7cc1a7a
Rohit bisht <rohitbisht@Varuns-MacBook-Pro.local>
2024-11-26 12:15:12 +0530

    wishlist page added

--- app/(tabs)/wishlist.tsx
+++ app/(tabs)/wishlist.tsx
@@ -1,6 +1,6 @@
 import { router } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useSelector } from "react-redux";
 
 const wishlist = () => {
@@ -8,26 +8,78 @@ const wishlist = () => {
     (state) => state.productSlice.wishlistProducts
   );
   console.log(wishlishItems[0]);
-  const handlePress = () => {
-    router.push("/detailsPage");
+
+  const handlePress = (id:number) => {
+    console.log("the id is :", id);
+    router.push({pathname:'/detailsPage',params:{id}});
+
   };
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={handlePress}>
-        <Text>wishlist</Text>
-        <Text>{wishlishItems.length}</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView>
+      <ScrollView  contentContainerStyle={styles.scrollContainer}>
+        {wishlishItems.map((product:any) => (
+          <TouchableOpacity
+            key={product.id}
+            style={styles.card}
+            onPress={() => handlePress(product.id)} 
+          >
+            <View>
+              <Image style={styles.img} source={{ uri: product.img }} />
+              <Text style={styles.heading}>{product.name}</Text>
+              {/* <Text style={styles.description}>{product.description}</Text> */}
+              <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+              {/* <Text style={styles.rating}>Rating: {product.rating} ★</Text> */}
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  scrollContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    paddingHorizontal: 8,
+    marginTop: 16,
+  },
+  card: {
+    width: "48%",
+    marginBottom: 16,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    borderRadius: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  img: {
+    width: "100%",
+    height: 160,
+    resizeMode: "contain",
+    marginBottom: 8,
+  },
+  heading: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 4,
+  },
+  description: {
+    fontSize: 10,
+    color: "#555",
+  },
+  price: {
+    marginTop: 10,
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#F83758",
+  },
+  rating: {
+    marginTop: 5,
+    fontSize: 12,
+    color: "#777",
   },
 });
-
-export default wishlist;
+export default wishlist; 
