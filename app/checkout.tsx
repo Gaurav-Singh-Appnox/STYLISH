2172954362ce229107c0ecf4ac26eadf18494c6e
Rohit bisht <rohitbisht@Varuns-MacBook-Pro.local>
2024-11-27 14:33:41 +0530

    added checkout page

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -1,12 +1,127 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import HorizontalLine from "@/components/common/HorizontalLine";
+import Button from "@/components/common/Button";
 
 export default function checkout() {
+  const cartItems = useSelector((state) => state.cart.items);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+
   return (
-    <View>
-      <Text>checkout</Text>
-    </View>
-  )
+    <ScrollView style={styles.container}>
+      <View style={styles.itemContainer}>
+        {cartItems.map((item) => (
+          <View key={item.id} style={styles.cartItem}>
+            <Image source={{ uri: item.img }} style={styles.productImage} />
+            <View style={styles.productDetails}>
+              <Text style={styles.productName}>{item.name}</Text>
+              <Text style={styles.description}>{item.description}</Text>
+              {/* <Text style={styles.productPrice}>${item.price.toFixed(2)}</Text> */}
+              {/* <Text style={styles.productQuantity}>
+                Total Quantity: {item.quantity}
+              </Text> */}
+              <Text style={styles.delivery}>
+                Devlivery by <Text style={{ fontWeight: 500 }}>10May20XX</Text>{" "}
+              </Text>
+            </View>
+          </View>
+        ))}
+      </View>
+      {/* <HorizontalLine /> */}
+      <View style={{ marginBottom: 10 }}>
+        <Text style={styles.heading}>Order Payment Details</Text>
+
+        <View style={styles.paymentText}>
+          <Text style={styles.innerText}>Order Amount</Text>
+          <Text style={{}}>${totalAmount}</Text>
+        </View>
+        <View style={styles.paymentText}>
+          <Text style={styles.innerText}>Convinience</Text>
+          <Text style={styles.productPrice}>Know more , add coupon</Text>
+        </View>
+        <View style={styles.paymentText}>
+          <Text style={styles.innerText}>Delivery Fee</Text>
+          <Text style={styles.productPrice}>Free</Text>
+        </View>
+      </View>
+      <HorizontalLine />
+      <View style={[styles.paymentText, { paddingHorizontal: 10 }]}>
+        <Text style={styles.heading}>Order Total </Text>
+        <Text style={styles.heading}>$ {totalAmount}</Text>
+      </View>
+      <HorizontalLine />
+      <View style={{ paddingTop: 40 }}>
+        <Button title={"Procced to Payment"} />
+      </View>
+    </ScrollView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { paddingHorizontal: 16 },
+  itemContainer: {},
+
+  cartItem: {
+    paddingVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+    // paddingBottom: 16,
+  },
+  productImage: {
+    borderRadius: 10,
+    width: 120,
+    height: 125,
+    resizeMode: "contain",
+    marginRight: 16,
+  },
+  productDetails: {
+    flex: 1,
+  },
+  productName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  description: {
+    paddingTop: 10,
+    fontSize: 14,
+  },
+  delivery: {
+    paddingTop: 10,
+  },
+  heading: {
+    paddingVertical: 30,
+    // paddingHorizontal: 22,
+    fontSize: 20,
+    fontWeight: 500,
+  },
+  innerText: { fontSize: 16 },
+  paymentText: {
+    justifyContent: "space-between",
+    backgroundColor: "",
+    // paddingHorizontal: 20,
+    flexDirection: "row",
+    paddingVertical: 10,
+  },
+  productPrice: {
+    fontSize: 16,
+    textDecorationColor: "underline",
+    fontWeight: 400,
+    color: "#F83758",
+  },
+  productQuantity: {
+    fontSize: 14,
+    color: "#777",
+  },
+});
--- components/specific/CircleSlider.tsx
+++ components/specific/CircleSlider.tsx
@@ -1,5 +1,13 @@
-import { ScrollView, Image, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {
+  ScrollView,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { router } from "expo-router";
 
 export default function CircleSlider() {
   return (
@@ -9,34 +17,78 @@ export default function CircleSlider() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.circleSlider}
       >
-        <View style={styles.card}>
-          <Image source={require('../../assets/images/Beauty.png')} style={{ height: 56, width: 56 }} />
-          <Text>Beauty</Text>
-        </View>
-        <View style={styles.card}>
-          <Image source={require('../../assets/images/fashion.png')} style={{ height: 56, width: 56 }} />
-          <Text>Fashion</Text>
-        </View>
-        <View  style={styles.card}>
-          <Image source={require('../../assets/images/kids.png')} style={{ height: 56, width: 56 }} />
-          <Text>Kids</Text>
-        </View>
-        <View style={styles.card}>
-          <Image source={require('../../assets/images/mens.png')} style={{ height: 56, width: 56 }} />
-          <Text>Mens</Text>
-        </View>
-        <View style={styles.card}>
-          <Image source={require('../../assets/images/women.png')} style={{ height: 56, width: 56 }} />
-          <Text>Womens</Text>
-        </View>
-        <View style={styles.card}>
-          <Image source={require('../../assets/images/mens.png')} style={{ height: 56, width: 56 }} />
-          <Text>Winters</Text>
-        </View>
-        <View style={styles.card}>
-          <Image source={require('../../assets/images/women.png')} style={{ height: 56, width: 56 }} />
-          <Text>Summers</Text>
-        </View>
+        <TouchableOpacity onPress={() => router.push("/(tabs)/shop")}>
+          
+          <View style={styles.card}>
+            <Image
+              source={require("../../assets/images/women.png")}
+              style={{ height: 56, width: 56 }}
+            />
+            <Text>Womens</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push("/(tabs)/shop")}>
+          
+          <View style={styles.card}>
+            <Image
+              source={require("../../assets/images/mens.png")}
+              style={{ height: 56, width: 56 }}
+            />
+            <Text>Mens</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push("/(tabs)/shop")}>
+          
+          <View style={styles.card}>
+            <Image
+              source={require("../../assets/images/fashion.png")}
+              style={{ height: 56, width: 56 }}
+            />
+            <Text>Fashion</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push("/(tabs)/shop")}>
+          <View style={styles.card}>
+            <Image
+              source={require("../../assets/images/mens.png")}
+              style={{ height: 56, width: 56 }}
+            />
+            <Text>Winters</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push("/(tabs)/shop")}>
+          <View style={styles.card}>
+            <Image
+              source={require("../../assets/images/Beauty.png")}
+              style={{ height: 56, width: 56 }}
+            />
+            <Text>Beauty</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push("/(tabs)/shop")}>
+          <View style={styles.card}>
+            <Image
+              source={require("../../assets/images/kids.png")}
+              style={{ height: 56, width: 56 }}
+            />
+            <Text>Kids</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push("/(tabs)/shop")}>
+          <View style={styles.card}>
+            <Image
+              source={require("../../assets/images/women.png")}
+              style={{ height: 56, width: 56 }}
+            />
+            <Text>Summers</Text>
+          </View>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
@@ -44,20 +96,20 @@ export default function CircleSlider() {
 
 const styles = StyleSheet.create({
   mainContainer: {
-    backgroundColor: '',
-    width: '100%',
+    backgroundColor: "",
+    width: "100%",
     height: 90,
     marginTop: 16,
   },
   circleSlider: {
     gap: 12,
     paddingHorizontal: 16,
-    alignItems: 'center',
+    alignItems: "center",
   },
   card: {
-    backgroundColor: '',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 8,
     borderRadius: 10,
     width: 80,
